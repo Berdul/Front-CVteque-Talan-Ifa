6502a2576229cba40d10d5b50a86450d38f93e26
Bertrand Verdun <bertrand.verdun@users.noreply.github.com>
2020-05-13 09:29:36 +0200

    profile model set to basic

--- src/app/model/profile.model.ts
+++ src/app/model/profile.model.ts
@@ -1,18 +1,8 @@
 export class Profile {
-
-	public links: string[];
-	public photoFileName: string;
-	public profilePassword: string;
 	
 	constructor(public firstName : string,
 				public lastName : string,
-				public nationality: string,
-				public dateOfBirth: string,
-				public adress: string,
-				public zip: number,
-				public phoneNumber: number,
-				public email: string,
-				public profileMainType: number){}
-
+				public age : number,
+				public technos : string[] ){}
 
 }
